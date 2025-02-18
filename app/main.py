98a51770cca7423f652dad58d5f0f23b8af4b315
Debarati Datta <debaratidatta.77star@gmail.com>
2025-02-18 10:55:46 -0800

    origin

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app.include_router(newsapi.router)
 app.include_router(gemini.router)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
